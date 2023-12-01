88a3fc97fb861ee6d81b888cfa56ae9da0cb1055
WhiteWolf47 <bhandari2003anurag@gmail.com>
2023-12-01 10:53:00 +0530

    changes in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,21 @@ from file_checker import checkFile
 st.title("MrExeSaver")
 
 file = st.file_uploader("Upload a file to check for malwares:", accept_multiple_files=True)
-if(st.button("Start Detection")):
+option = st.selectbox(
+    'Choose a Model',
+    ('RandomForest', 'DecisionTree', 'KNeighbors', 'AdaBoost','SGD','ExtraTrees','GaussianNB'))
+
+if st.button("Start Detection"):
     if len(file):
-        with st.spinner("Checking..."):
-            for i in file:
-                open('malwares/tempFile', 'wb').write(i.getvalue())
-                legitimate = checkFile("malwares/tempFile")
-                os.remove("malwares/tempFile")
-                if legitimate:
-                    st.write(f"File {i.name} seems *LEGITIMATE*!")
-                else:
-                    st.markdown(f"File {i.name} is probably a **MALWARE**!!!")
+        if not option:
+            st.error('Please select an option')
+        else:
+            with st.spinner("Checking..."):
+                for i in file:
+                    open('malwares/tempFile', 'wb').write(i.getvalue())
+                    legitimate = checkFile("malwares/tempFile")
+                    os.remove("malwares/tempFile")
+                    if legitimate:
+                        st.write(f"File {i.name} seems *LEGITIMATE*!")
+                    else:
+                        st.markdown(f"File {i.name} is probably a **MALWARE**!!!")
